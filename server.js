de5fe940664fdebfdefaa2e9b5aedbffde7af55a
Patience Nduku <117981660+patience9kyalo@users.noreply.github.com>
2024-10-12 17:14:32 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io')
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5170',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    socket.on('sendMessage', (message) => {
+        console.log('Message received from server:', message);
+
+        // Emit response back to client
+        io.emit('receiveMessage', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+server.listen(4000, () => {
+    console.log('Server listening on port 4000');
+});
+
